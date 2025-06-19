00ec6e5a95811b7c1583e6b6533b042559c84bef
Tim Thomas <tim.thomas.austin@gmail.com>
2025-06-19 16:38:11 -0500

    fix build dir

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,16 @@
 import react from '@vitejs/plugin-react';
 
 export default {
-  build: { outDir: 'build' },
+  build: {
+    outDir: '../build',
+    emptyOutDir: true,
+  },
   base: '/',
   plugins: [react()],
   root: __dirname,
   optimizeDeps: {
-    include: [ 
-      "@mui/material/Unstable_Grid2"
+    include: [
+      '@mui/material/Unstable_Grid2',
     ],
   },
 };
